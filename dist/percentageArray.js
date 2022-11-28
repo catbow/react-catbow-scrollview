60fba69b055668366fcfcd7ee62c54e5cd3ffff7
kimjuno97 <jh241093@gmail.com>
2022-11-28 16:07:12 +0900

    feat: 타입 선언 파일 추가
    feat: 모듈 선언 파일 추가
    
    feat: babel compact: true 설정
    
    feat: dist 폴더 하위 줄바꿈 제거

--- dist/percentageArray.js
+++ dist/percentageArray.js
@@ -1,9 +1,1 @@
-const percentageArray = () => {
-  const arry = [];
-  let count = 100;
-  for (let i = 0; i <= count; i++) {
-    arry.push(i / 100);
-  }
-  return arry;
-};
-export default percentageArray;
+const percentageArray=()=>{const arry=[];let count=100;for(let i=0;i<=count;i++){arry.push(i/100);}return arry;};export default percentageArray;
