4283eff7033373776205b2b51628f3ffd2dedb39
kimjuno97 <jh241093@gmail.com>
2022-12-01 17:54:09 +0900

    docs: 불필요한 파일 제거및 gitignore 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ScrollView from './lib/ScrollView';
 
-// const root = ReactDOM.createRoot(
-// 	document.getElementById('root') as HTMLElement
-// );
-// root.render(<React.StrictMode></React.StrictMode>);
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+);
+root.render(<React.StrictMode></React.StrictMode>);
