bcb885c5965d54bfcb745836fe7c16b43acb3b93
kimjuno97 <jh241093@gmail.com>
2022-12-02 11:20:48 +0900

    chore: tsconfig 컴파일 설정에 d.ts 파일 추가하기 등록 및 package.json build 명령어로 자동화

--- dist/percentageArray.js
+++ dist/percentageArray.js
@@ -1,1 +1,1 @@
-const percentageArray=()=>{const arry=[];let count=100;for(let i=0;i<=count;i++){arry.push(i/100);}return arry;};export default percentageArray;
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});const percentageArray=()=>{const arry=[];let count=100;for(let i=0;i<=count;i++){arry.push(i/100);}return arry;};exports.default=percentageArray;
