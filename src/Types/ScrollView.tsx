31260c335223c79f84c353f699b186c118945255
kimjuno97 <jh241093@gmail.com>
2022-12-03 12:50:17 +0900

    publish: 0.1.5, JSDoc 추가, Readme 수정

--- src/Types/ScrollView.tsx
+++ src/Types/ScrollView.tsx
@@ -2,14 +2,31 @@ import React, { useEffect, useRef, useState } from 'react';
 import percentageArray from './percentageArray';
 
 interface TypeScrollView {
-	videoImageCount: number;
 	imgUrl: string;
 	startNum: number;
+	videoImageCount: number;
 	extension: string;
 	scrollAreaY: string;
 	viewPort?: any;
 }
-
+/**
+ *
+ * @param option
+ * @property {imgUrl} string -> Address before image
+ * @property {videoImageCount} number -> Total-number-of-images
+ * @property {startNum} number -> Image-path-start-number,
+ * @property {extension} string -> Available-with-any-image-extension,
+ * @property {scrollAreaY} px -> scrollArea-only-px,
+ * @property {viewPort?} imgStyle -> all-imgTag-styles-available
+ * @property Example {
+ *	imgUrl: './images/004/',
+ * 	extension: '.jpg', startNum: 10000,
+ *  videoImageCount: 754, scrollAreaY: '50000px',
+ *		viewPort: {
+ *			width: '500px',
+ *		},
+ *	}
+ */
 export default function ScrollView({ option }: { option: TypeScrollView }) {
 	const {
 		videoImageCount,
