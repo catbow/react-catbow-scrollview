a2bcc34fde96b8d8e80261e77e85d97cd42c2dc2
kimjuno97 <jh241093@gmail.com>
2022-12-07 10:59:44 +0900

    docs: REAMD.md issues 관련 문구 추가

--- dist/percentageArray.js
+++ dist/percentageArray.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});const percentageArray=()=>{const arry=[];let count=100;for(let i=0;i<=count;i++){arry.push(i/100);}return arry;};exports.default=percentageArray;
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});var percentageArray=function percentageArray(){var arry=[];var count=100;for(var i=0;i<=count;i++){arry.push(i/100);}return arry;};exports.default=percentageArray;
